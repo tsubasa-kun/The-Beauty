8892553d33facb277d084d625885787197a91ecd
Cookie <tsubasa-kun@foxmail.com>
2016-08-10 16:07:00 +0800

    启动页背景随机彩色

--- app/src/main/java/com/love_cookies/beauty/view/activity/SplashActivity.java
+++ app/src/main/java/com/love_cookies/beauty/view/activity/SplashActivity.java
@@ -1,15 +1,18 @@
 package com.love_cookies.beauty.view.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.love_cookies.beauty.R;
 import com.love_cookies.cookie_library.activity.BaseActivity;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
 
 /**
  * Created by xiekun on 2016/7/6 0006.
@@ -18,6 +21,10 @@ import org.xutils.view.annotation.ContentView;
  */
 @ContentView(R.layout.activity_splash)
 public class SplashActivity extends BaseActivity {
+
+    @ViewInject(R.id.root_view)
+    private RelativeLayout rootView;
+    private String[] colorList;
 
     private final int SPLASH_DISPLAY_DURATION = 1500;//启动页显示时长
     private Looper looper = Looper.myLooper();
@@ -36,6 +43,9 @@ public class SplashActivity extends BaseActivity {
      */
     @Override
     public void initWidget(Bundle savedInstanceState) {
+        colorList = getResources().getStringArray(R.array.color_list);
+        int index = (int)(Math.random() * (colorList.length - 1)) + 1;
+        rootView.setBackgroundColor(Color.parseColor(colorList[index]));
         handler.postDelayed(runnable, SPLASH_DISPLAY_DURATION);
     }
 
