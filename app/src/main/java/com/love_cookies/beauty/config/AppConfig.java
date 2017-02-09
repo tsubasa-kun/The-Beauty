a3d20959f465ea6890cfcade37256e106f82f7df
Cookie <tsubasa-kun@foxmail.com>
2017-02-09 13:23:54 +0800

    修正API请求地址

--- app/src/main/java/com/love_cookies/beauty/config/AppConfig.java
+++ app/src/main/java/com/love_cookies/beauty/config/AppConfig.java
@@ -7,5 +7,7 @@ package com.love_cookies.beauty.config;
  */
 public class AppConfig {
     //来自干货集中营的福利api
-    public static final String API = "http://gank.io/api/data/福利/10/";
+//    public static final String API = "http://gank.io/api/data/福利/10/";
+    public static final String API = "http://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/";
 }
+
